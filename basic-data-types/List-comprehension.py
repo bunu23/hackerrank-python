da7fe8736b2f3148bc2681c77700fa75e61a9c0f
Bunu <bunubhattarai39@gmail.com>
2024-04-03 23:01:40 -0400

    py

---
+++ basic-data-types/List-comprehension.py
@@ -0,0 +1,14 @@
+
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+
+coordinates = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1)]
+
+
+filtered_coordinates = [coord for coord in coordinates if sum(coord) != n]
+
+
+print(filtered_coordinates)
