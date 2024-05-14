0bc24185f283220548b4279e9b9fd57ca70b3371
Bunu <bunubhattarai39@gmail.com>
2024-05-13 20:58:35 -0400

    py

---
+++ sets/union/Union.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+
+english_subs = set(map(int, input().split()))
+
+
+m = int(input())
+
+french_subs = set(map(int, input().split()))
+
+total_subs = len(english_subs.union(french_subs))
+
+print(total_subs)
