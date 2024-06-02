7808e38866e8543e01a0e2d27700fe6029e3b596
Bunu <bunubhattarai39@gmail.com>
2024-06-02 15:05:18 -0400

    py

---
+++ math/mod-divmod/Mod.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+a = int(input())
+b = int(input())
+
+
+integer_division = a // b
+
+
+modulo_result = a % b
+
+
+divmod_result = divmod(a, b)
+
+
+print(integer_division)
+print(modulo_result)
+print(divmod_result)
