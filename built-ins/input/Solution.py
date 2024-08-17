322853d3c1e9692bbdabdda5a9ad3f5b25b12830
Bunu <bunubhattarai39@gmail.com>
2024-08-16 22:17:49 -0400

    py

---
+++ built-ins/input/Solution.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+x, k = map(int, input().split())
+
+
+polynomial = input()
+
+
+result = eval(polynomial)
+
+
+print(result == k)
